5e700e8811137e836ea4c840896a2bc3256af495
Pritoj Singh <pritojs@amazon.com>
2024-07-01 14:37:34 +0000

    Treesitter plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,52 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+      configs.setup({
+          ensure_installed = { 
+		-- Shell stuff
+                "bash",
+
+                -- Lua stuff
+                "lua",
+
+                -- WebDev
+                "html",
+                "css",
+                "javascript",
+                "typescript",
+                "scss",
+                "tsx",
+
+                -- Config Formats
+                "yaml",
+                "toml",
+                "json5",
+
+		-- Utilities
+		"jq",
+
+                "ruby",
+
+                "go",
+                "dockerfile",
+                "c",
+
+                "markdown",
+                "markdown_inline",
+
+                "smithy",
+	},
+          sync_install = false,
+          highlight = { 
+		enable = true,
+				disable = {
+                        "python",
+                },
+
+	},
+          indent = { enable = true },  
+        })
+    end
+ }
