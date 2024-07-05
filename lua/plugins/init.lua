95c0ab8dd4c7c55780e56faf5299612c2363a050
Pritoj Singh <pritojs@amazon.com>
2024-07-05 12:40:40 +0000

    feat: lsp signature help completion

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,6 +50,7 @@ return {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp-signature-help'
     },
     config = function()
       require "config.nvim-cmp"
