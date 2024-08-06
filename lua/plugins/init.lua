88d3a71dca10e090e1434f36258399bead8cd338
Pritoj Singh <pritojs@amazon.com>
2024-08-06 12:11:05 +0000

    🔧: Use tokyonight

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,14 +3,14 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd 'colorscheme tokyonight'
+    end
   },
   {
     "EdenEast/nightfox.nvim",
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd 'colorscheme carbonfox'
-    end
   },
   {
     "nvim-tree/nvim-web-devicons"
