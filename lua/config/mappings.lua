44925fdf5da02a2179295da57a2983e70bdeae12
Pritoj Singh <pritojs@amazon.com>
2025-02-07 12:32:23 +0000

    Add map for copying file path

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -24,3 +24,10 @@ nmap("<leader>th", "<cmd> Telescope colorscheme<cr>", "Select [th]eme")
 -- Buffer switching.
 nmap("<leader><S-Tab>", ":BufferLineCyclePrev<CR>", "Previous Buffer")
 nmap("<leader><Tab>", ":BufferLineCycleNext<CR>", "Next Buffer")
+
+local function insertFullPath()
+  local filepath = vim.fn.expand('%')
+  vim.fn.setreg('+', filepath) -- write to clippoard
+end
+
+nmap('<leader>cfp', insertFullPath, '[C]opy [F]ile [P]ath')
