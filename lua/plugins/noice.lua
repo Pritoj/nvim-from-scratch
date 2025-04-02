c655dc00896c72ad594c28da95d1f83a4f440a09
Pritoj Singh <pritojs@amazon.com>
2025-04-02 09:56:09 +0000

    ✨ More goodies from snacks

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,20 +1,16 @@
 local M = -- lazy.nvim
 {
+
   "folke/noice.nvim",
   event = "VeryLazy",
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-  }
+  },
 }
 
 M.config = function()
   local noice = require("noice")
-  require('config.notify')
 
   noice.setup({
     lsp = {
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,19 +5,61 @@ local M = {
   ---@type snacks.Config
   opts = {
     explorer = { enabled = true },
+    toggle = { enabled = true },
+    input = { enabled = true },
+    scroll = { enabled = true },
+    words = { enabled = true },
+    notifier = { enabled = true },
+    dim = { enabled = true },
+
   },
 
-  ---@type snacks.Keys
   keys = {
     -- Files
-    { "<leader>ff",  function() Snacks.picker.smart() end,           desc = "[F]ind [F]iles smartly" },
-    { "<leader>ffd", function() Snacks.picker.files() end,           desc = "[F]ind [F]iles [d]umbly" },
-    { "<leader>fb",  function() Snacks.picker.buffers() end,         desc = "[F]ind [B]uffers" },
-    { "<leader>fw",  function() Snacks.picker.grep() end,            desc = "[F]ind [W]ord" },
-    { "<leader>ch",  function() Snacks.picker.command_history() end, desc = "[C]ommand [H]istory" },
-    { "<leader>nh",  function() Snacks.picker.notifications() end,   desc = "[N]otification [H]istory" },
-    { "<leader>e",   function() Snacks.explorer() end,               desc = "File [E]xplorer" },
+    { "<leader>ff",      function() Snacks.picker.smart() end,            desc = "[F]ind [F]iles smartly" },
+    { "<leader><space>", function() Snacks.picker.files() end,            desc = "Find Files dumbly" },
+    { "<leader>fb",      function() Snacks.picker.buffers() end,          desc = "[F]ind [B]uffers" },
+    { "<leader>e",       function() Snacks.explorer() end,                desc = "File [E]xplorer" },
+    { "<leader>fw",      function() Snacks.picker.grep() end,             desc = "[F]ind [W]ord" },
+
+    -- History
+    { "<leader>ch",      function() Snacks.picker.command_history() end,  desc = "[C]ommand [H]istory" },
+    { "<leader>nh",      function() Snacks.picker.notifications() end,    desc = "[N]otification [H]istory" },
+
+    -- References
+    { "<leader>nr",      function() Snacks.words.jump(vim.v.count1) end,  desc = "[N]ext [R]eference",      mode = { "n", "t" } },
+    { "<leader>pr",      function() Snacks.words.jump(-vim.v.count1) end, desc = "[P]rev [R]eference",      mode = { "n", "t" } },
   }
 }
+
+M.init = function()
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "VeryLazy",
+    callback = function()
+      -- Setup some globals for debugging (lazy-loaded)
+      _G.dd = function(...)
+        Snacks.debug.inspect(...)
+      end
+      _G.bt = function()
+        Snacks.debug.backtrace()
+      end
+      vim.print = _G.dd -- Override print to use snacks for `:=` command
+
+      -- Create some toggle mappings
+      Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
+      Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
+      Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+      Snacks.toggle.diagnostics():map("<leader>ud")
+      Snacks.toggle.line_number():map("<leader>ul")
+      Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map(
+        "<leader>uc")
+      Snacks.toggle.treesitter():map("<leader>uT")
+      Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
+      Snacks.toggle.inlay_hints():map("<leader>uh")
+      Snacks.toggle.indent():map("<leader>ug")
+      Snacks.toggle.dim():map("<leader>uD")
+    end,
+  })
+end
 
 return M
